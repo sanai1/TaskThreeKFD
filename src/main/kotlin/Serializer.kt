7971afdf4573501a0d00aae08a111b7643122322
aleksandr <sanek.v.07@gmail.com>
2025-03-07 21:10:22 +0300

    update serialize in Serializer.kt - second start super word

--- src/main/kotlin/Serializer.kt
+++ src/main/kotlin/Serializer.kt
@@ -1,59 +1,72 @@
-package org.example
-
+import com.google.gson.Gson
 import kotlinx.coroutines.*
-import kotlin.reflect.KClass
-import kotlin.reflect.KType
 import kotlin.reflect.full.memberProperties
-import kotlin.reflect.jvm.javaType
-import kotlin.reflect.jvm.jvmErasure
 
-suspend fun <T : Any> serialize(obj: T): String = coroutineScope {
-    if (obj::class.isList()) {
-        val list = obj as List<*>
-        val job = list.map { item ->
-            async {
-                if (item != null) serialize(item) else "null"
-            }
-        }.awaitAll()
-        return@coroutineScope "[" + job.joinToString(",") + "]"
-    }
-    val properties = obj::class.memberProperties
+private const val BATCH_SIZE = 100
 
-    val serializedProperties = properties.map { prop ->
-        val value = prop.getter.call(obj)
-
-        when {
-            value == null -> "\"${prop.name}\":null"
-            prop.returnType.isPrimitiveOrString() -> "\"${prop.name}\":\"${value}\""
-            prop.returnType.isList() -> {
-                val list = value as List<*>
-                val serializedList = list.map { item ->
-                    async {
-                        item?.let { serialize(it) } ?: "null"
+suspend fun <T : Any> asyncSerializeObject(obj: T): String = coroutineScope {
+    when (obj) {
+        is List<*> -> {
+            val deferredResults = obj.chunked(BATCH_SIZE).map { batch ->
+                async(Dispatchers.Default) {
+                    batch.map { item ->
+                        if (item != null) {
+                            asyncSerializeObject(item)
+                        } else {
+                            "null"
+                        }
+                    }.joinToString(",")
+                }
+            }.awaitAll().joinToString(",")
+            "[$deferredResults]"
+        }
+        is String, is Number, is Boolean -> Gson().toJson(obj)
+        else -> {
+            val kClass = obj::class
+            val properties = kClass.memberProperties
+            val fields = properties.map { property ->
+                async(Dispatchers.Default) {
+                    val fieldName = property.name
+                    val fieldValue = property.getter.call(obj)
+                    val serializedValue = if (fieldValue != null) {
+                        asyncSerializeObject(fieldValue)
+                    } else {
+                        "null"
                     }
-                }.awaitAll().joinToString(",")
-                "\"${prop.name}\":[$serializedList]"
-            }
-
-            else -> "\"${prop.name}\":${serialize(value)}" // Для объектов
+                    """"$fieldName":$serializedValue"""
+                }
+            }.awaitAll().joinToString(",")
+            "{$fields}"
         }
     }
-
-    return@coroutineScope "{${serializedProperties.joinToString(",")}}"
 }
 
-fun KType.isPrimitiveOrString(): Boolean {
-    val simpleName = listOf("Int", "Long", "Float", "Double", "Boolean", "Char", "String")
-    return when (this.jvmErasure.simpleName) {
-        in simpleName -> true
-        else -> false
+fun <T : Any> serializeObject(obj: T): String =
+    when (obj) {
+        is List<*> -> {
+            val results = obj.map { item ->
+                if (item != null) {
+                    serializeObject(item)
+                } else {
+                    "null"
+                }
+            }
+            "[${results.joinToString(",")}]"
+        }
+
+        is String, is Number, is Boolean -> Gson().toJson(obj)
+        else -> {
+            val kClass = obj::class
+            val fields = kClass.memberProperties.map { property ->
+                val fieldName = property.name
+                val fieldValue = property.getter.call(obj)
+                val serializedValue = if (fieldValue != null) {
+                    serializeObject(fieldValue)
+                } else {
+                    "null"
+                }
+                """"$fieldName":$serializedValue"""
+            }
+            "{${fields.joinToString(",")}}"
+        }
     }
-}
-
-fun KType.isList(): Boolean {
-    return this.javaType.typeName.contains("java.util.List")
-}
-
-fun KClass<*>.isList(): Boolean {
-    return this.java.typeName.contains("java.util.List")
-}

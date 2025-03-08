25d898dbd00ab3ca1789f3c5ca102a84a22305bb
aleksandr <sanek.v.07@gmail.com>
2025-03-08 22:57:03 +0300

    update first version Deserializer.kt (no work)

--- src/main/kotlin/org/example/Deserializer.kt
+++ src/main/kotlin/org/example/Deserializer.kt
@@ -2,78 +2,60 @@ import kotlinx.coroutines.*
 import kotlinx.serialization.json.*
 import kotlin.reflect.KClass
 import kotlin.reflect.KType
-import kotlin.reflect.full.primaryConstructor
-import kotlin.reflect.full.memberProperties
+import kotlin.reflect.full.*
 
-// Асинхронный десериализатор
 object AsyncDeserializer {
-
-    // Основная функция десериализации
     suspend fun <T : Any> deserializeAsync(json: String, clazz: KClass<T>): T = coroutineScope {
         val jsonElement = Json.parseToJsonElement(json)
         return@coroutineScope deserializeJsonElement(jsonElement, clazz) as T
     }
 
-    // Рекурсивная функция для десериализации JsonElement
-    private suspend fun <T : Any> deserializeJsonElement(jsonElement: JsonElement, clazz: KClass<T>): Any? = coroutineScope {
-        when (jsonElement) {
-            is JsonObject -> {
-                // Получаем первичный конструктор класса
-                val constructor = clazz.primaryConstructor
-                    ?: throw IllegalArgumentException("Class ${clazz.simpleName} must have a primary constructor")
-
-                // Параллельно десериализуем все поля
-                val args = constructor.parameters.associateWith { param ->
-                    val fieldName = param.name
-                    val fieldValue = jsonElement[fieldName]
-                    if (fieldValue != null) {
-                        // Рекурсивно десериализуем значение поля
+    private suspend fun <T : Any> deserializeJsonElement(jsonElement: JsonElement, clazz: KClass<T>): Any? =
+        coroutineScope {
+            when (jsonElement) {
+                is JsonObject -> {
+                    val constructor = clazz.primaryConstructor
+                        ?: throw IllegalArgumentException("Class ${clazz.simpleName} must have a primary constructor")
+                    val args = constructor.parameters.associateWith { param ->
+                        val fieldName = param.name
+                        val fieldValue = jsonElement[fieldName]!!
                         async { deserializeJsonElement(fieldValue, getClassForType(param.type)) }
-                    } else {
-                        // Если поле отсутствует, используем значение по умолчанию (null)
-                        async { null }
+                    }.mapValues { it.value.await() }
+                    constructor.callBy(args)
+                }
+
+                is JsonArray -> {
+                    val elementClass = getClassForType(clazz)
+                    val jobs = jsonElement.map { element ->
+                        async { deserializeJsonElement(element, elementClass) }
                     }
-                }.mapValues { it.value.await() } // Ожидаем завершения всех задач
+                    jobs.awaitAll()
+                }
 
-                // Создаем объект с использованием десериализованных значений
-                constructor.callBy(args)
-            }
-            is JsonArray -> {
-                // Определяем тип элементов массива
-                val elementClass = getClassForType(clazz)
-                // Десериализуем каждый элемент массива параллельно
-                val jobs = jsonElement.map { element ->
-                    async { deserializeJsonElement(element, elementClass) }
+                is JsonPrimitive -> {
+                    when {
+                        jsonElement.isString -> jsonElement.content
+                        jsonElement.booleanOrNull != null -> jsonElement.boolean
+                        jsonElement.intOrNull != null -> jsonElement.int
+                        jsonElement.longOrNull != null -> jsonElement.long
+                        jsonElement.doubleOrNull != null -> jsonElement.double
+                        jsonElement.toString() == "null" -> null
+                        else -> throw IllegalArgumentException("Unsupported primitive type: $jsonElement")
+                    }
                 }
-                // Ожидаем завершения всех задач и возвращаем список
-                jobs.awaitAll()
+
+                else -> throw IllegalArgumentException("Unsupported JSON element type: $jsonElement")
             }
-            is JsonPrimitive -> {
-                // Десериализация примитивных значений
-                when {
-                    jsonElement.isString -> jsonElement.content
-                    jsonElement.booleanOrNull != null -> jsonElement.boolean
-                    jsonElement.intOrNull != null -> jsonElement.int
-                    jsonElement.longOrNull != null -> jsonElement.long
-                    jsonElement.doubleOrNull != null -> jsonElement.double
-                    jsonElement.toString() == "null" -> null
-                    else -> throw IllegalArgumentException("Unsupported primitive type: $jsonElement")
-                }
-            }
-            else -> throw IllegalArgumentException("Unsupported JSON element type: $jsonElement")
         }
-    }
 
-    // Вспомогательная функция для получения KClass из типа
     private fun <T : Any> getClassForType(type: KClass<T>): KClass<*> {
         return when (type) {
-            List::class -> Any::class // Для списков используем Any как тип элементов
+            List::class -> Any::class
             else -> type
         }
     }
 
     private fun getClassForType(type: KType): KClass<*> {
-        // Если тип является обобщенным (например, List<String>), возвращаем его raw-тип (List::class)
         return when (val classifier = type.classifier) {
             is KClass<*> -> classifier
             else -> throw IllegalArgumentException("Unsupported type: $type")
@@ -81,15 +63,13 @@ object AsyncDeserializer {
     }
 }
 
-// Пример класса для десериализации
 data class MyData(
     val field1: String,
     val field2: Int,
     val field3: List<MyData>,
-    val field4: MyData?
+    val field4: MyData?,
 )
 
-// Пример использования
 fun main() = runBlocking {
     val json = """
         {
